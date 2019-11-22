432fa445ee5da4b2b45a76fa4b5eca6df1b2f701
Kyle Anderson <kyleanderson@u.boisestate.edu>
2019-11-21 19:31:06 -0700

    Important message

--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-<php>Mike is gay!</php>
+<html>
+<head>
+    <title>
+        Call Center Management Simulator
+    </title>
+</head>
+<body>
+<?php
+    $name = "Jeffrey Epstein";
+    echo $name . " didn't kill himself.";
+?>
+</body>
+
+</html>
