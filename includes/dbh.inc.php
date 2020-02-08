916684e15e504591b3aa0c5696d87e2bd5ee4dc3
Kyle Anderson <kyleanderson@u.boisestate.edu>
2020-02-08 11:22:15 -0700

    Heroku

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,15 +1,15 @@
 <?php
 
-$servername="localhost";
-$dBUsername = "root";
-$dBPassword = "";
-$dBName = "loginsystem";
+//$servername="localhost";
+//$dBUsername = "root";
+//$dBPassword = "";
+//$dBName = "loginsystem";
 
 //TODO: Add in block for the remote server name
-//$servername = "us-cdbr-iron-east-05.cleardb.net";
-//$dBUsername = "b2b6bcdb20cf3a";
-//$dBPassword = "5dd350c0";
-//$dBName = "heroku_40e4688a774732a";
+$servername = "us-cdbr-iron-east-05.cleardb.net";
+$dBUsername = "b2b6bcdb20cf3a";
+$dBPassword = "5dd350c0";
+$dBName = "heroku_40e4688a774732a";
 
 $connection = mysqli_connect($servername, $dBUsername, $dBPassword, $dBName);
 
